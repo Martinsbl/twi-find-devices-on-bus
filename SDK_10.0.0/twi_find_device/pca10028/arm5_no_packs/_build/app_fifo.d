6acfd761d14b43a8344930f3bf12cb8759f22478
badcoffee <mbl@test.com>
2016-03-21 14:35:59 +0100

    clean up

--- SDK_10.0.0/twi_find_device/pca10028/arm5_no_packs/_build/app_fifo.d
+++
@@ -1,8 +0,0 @@
-.\_build\app_fifo.o: ..\..\..\..\..\components\libraries\fifo\app_fifo.c
-.\_build\app_fifo.o: ..\..\..\..\..\components\libraries\fifo\app_fifo.h
-.\_build\app_fifo.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\_build\app_fifo.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\_build\app_fifo.o: ..\..\..\..\..\components\drivers_nrf\nrf_soc_nosd\nrf_error.h
-.\_build\app_fifo.o: ..\..\..\..\..\components\libraries\util\app_util.h
-.\_build\app_fifo.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
-.\_build\app_fifo.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.2.0\Device\Include\compiler_abstraction.h
